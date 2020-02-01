ab107e9f3498cbc8eae7643dfa06627cff43e052
Norrey Okumu <norrey.okumu@cellulant.com>
2020-02-01 11:27:19 +0300

    Added intent extra for visual configuration

--- app/src/main/java/com/talklift/plugin/TalkLiftPluginMainActivity.java
+++ app/src/main/java/com/talklift/plugin/TalkLiftPluginMainActivity.java
@@ -2,6 +2,7 @@ package com.talklift.plugin;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -27,8 +28,17 @@ public class TalkLiftPluginMainActivity extends Activity {
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
+//                base_color,
+//                text_color,
+//                c_code,
+        Intent intent = getIntent();
+        String base_color = intent.getStringExtra("base_color");
+        String text_color = intent.getStringExtra("text_color");
+        String c_code = intent.getStringExtra("c_code");
+
+
         // This URL should be loaded automatically
-         mWebView.loadUrl("https://app.talklift.com/web-widget/1/?org_id=1");
+         mWebView.loadUrl("https://app.talklift.com/web-widget/1/?org_id=1&c_code="+c_code+"&base_color="+base_color+"&text_color="+text_color);
          mWebView.setWebViewClient(new TalkLiftWebViewClient());
     }
 
